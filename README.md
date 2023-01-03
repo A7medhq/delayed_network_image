764f67adf79fcfe92af78912bc20d72848f62303
A7medhq <a.m.hq98@gmail.com>
2023-01-04 01:26:17 +0200

    develop the code

--- README.md
+++ README.md
@@ -1,24 +1,23 @@
-<!--
-This README describes the package. If you publish this package to pub.dev,
-this README's contents appear on the landing page for your package.
+A package that creates a placeholder using an asset image that the user provides until the network image loads.
 
-For information about how to write a good package README, see the guide for
-[writing package pages](https://dart.dev/guides/libraries/writing-package-pages).
 
-For general information about developing packages, see the Dart guide for
-[creating packages](https://dart.dev/guides/libraries/create-library-packages)
-and the Flutter guide for
-[developing packages and plugins](https://flutter.dev/developing-packages).
--->
+|             | Android | iOS  |
+|-------------|---------|------|
+| **Support** | SDK 16+ | 9.0+ |
+
+<p>
+  <img src="https://i.ibb.co/64gKGTR/Media-230104-005312.gif"
+    alt="An animated image of the Android delayed image" height="400"/>
+</p>
 
 
 ## Features
 
-A package that creates a placeholder using an asset image that the user provides until the network image loads.
+Use an ```AssetImage``` while the ```NetworkImage``` downloading.
 
 ## Getting started
 
-import the package 
+import the package
 ```dart
 import 'package:delayed_network_image/delayed_network_image.dart';
 ```
--- lib/src/delayed_network_image.dart
+++ lib/src/delayed_network_image.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/material.dart';
 
 class DelayedNetworkImage extends StatefulWidget {
-  ImageProvider networkImage;
-  ImageProvider assetImage;
-  Widget? child;
+  // it's where the network image provider goes
+  final ImageProvider networkImage;
 
-  DelayedNetworkImage({
+  // it's where the asset image provider goes
+  final ImageProvider assetImage;
+
+  // it's where the child goes
+  final Widget? child;
+
+  const DelayedNetworkImage({
     Key? key,
     required this.assetImage,
     required this.networkImage,
@@ -17,10 +22,12 @@ class DelayedNetworkImage extends StatefulWidget {
 }
 
 class _DelayedNetworkImageState extends State<DelayedNetworkImage> {
+  // to check if the NetworkImage finished downloading
   bool _doneLoading = false;
 
   @override
   void initState() {
+    // The function that check the loading process
     widget.networkImage
         .resolve(const ImageConfiguration())
         .addListener(ImageStreamListener((ImageInfo imageInfo, bool syncCall) {
@@ -32,12 +39,9 @@ class _DelayedNetworkImageState extends State<DelayedNetworkImage> {
     super.initState();
   }
 
-  ImageProvider createImageProvider() {
-    return _doneLoading == true ? widget.networkImage : widget.assetImage;
-  }
-
   @override
   Widget build(BuildContext context) {
+    // Our custom Container with Decoration image that changes according to the downloading process
     return Container(
       decoration: BoxDecoration(
           image: DecorationImage(
